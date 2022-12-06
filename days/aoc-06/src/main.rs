23da1d2e857a125732d156246eeaf54e10511c76
Peter Cardwell-Gardner <burntproduce@gmail.com>
2022-12-06 19:14:47 +0000

    Aoc day 6

---
+++ days/aoc-06/src/main.rs
@@ -0,0 +1,67 @@
+#![feature(iter_array_chunks)]
+
+use iter_tools::Itertools;
+use std::collections::HashSet;
+
+fn main() {
+    let _example = include_str!("../input/example.txt");
+    let input = include_str!("../input/input.txt");
+
+    let mut set = HashSet::<char>::new();
+
+    //Part 1
+    let input: Vec<char> = input.chars().collect();
+    let index: usize = input
+        .iter()
+        .tuple_windows()
+        .enumerate()
+        .find_map(|(i, (a, b, c, d))| {
+            set.clear();
+            set.insert(*a);
+            set.insert(*b);
+            set.insert(*c);
+            set.insert(*d);
+            if set.len() == 4 {
+                return Some(i + 4);
+            } else {
+                return None;
+            }
+        })
+        .unwrap();
+
+    println!("First index {}", index);
+
+    //Pro tips
+    //window.iter().unique().count() instead of the set, but does the same internally anyway
+
+    //Part 2 - fine, make me do it properly
+    let index: usize = input
+        .windows(14)
+        .enumerate()
+        .find_map(|(i, items)| {
+            set.clear();
+            set.extend(items);
+            if set.len() == 14 {
+                return Some(i + 14);
+            } else {
+                return None;
+            }
+        })
+        .unwrap();
+
+    println!("Second index {}", index);
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[test]
+//     fn test_parse_move() {
+//         let raw = "move 13 from 9 to 1";
+//         let mov = parse_move(raw);
+//         assert_eq!(13, mov.count);
+//         assert_eq!(9, mov.from);
+//         assert_eq!(1, mov.to);
+//     }
+// }
